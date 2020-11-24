953f0dbf16a854910867b14d00a5e7b2152c618c
mike32n <mihaly32horvath@gmail.com>
2020-11-24 17:24:25 +0100

    glass issue type schemes

--- src/test/java/MainPage.java
+++ src/test/java/MainPage.java
@@ -1,7 +1,34 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class MainPage extends PageObject{
+
+    @FindBy(id = "create_link")
+    private WebElement createButton;
+
+    @FindBy(xpath = "//a[@id='header-details-user-fullname']/span/span/img")
+    private WebElement userIcon;
+
+    @FindBy(xpath = "//*[@id='log_out']")
+    private WebElement logoutButton;
+
+    @FindBy(xpath = "//*[@id='content']//strong")
+    private WebElement logoutMessage;
+
     public MainPage(WebDriver driver) {
         super(driver);
+    }
+
+    @Override
+    public void navigate(String URL) {
+        waitForVisibility(userIcon);
+        super.navigate(URL);
+    }
+
+    public void logout() {
+        clickOn(userIcon);
+        clickOn(logoutButton);
+        waitForVisibility(logoutMessage);
     }
 }
--- src/test/java/TestPlan.java
+++ src/test/java/TestPlan.java
@@ -7,22 +7,38 @@ import org.testng.annotations.Test;
 public class TestPlan {
     private final WebDriver driver = new ChromeDriver();
 
+    private LoginPage loginPage = new LoginPage(driver);
+    private MainPage mainPage = new MainPage(driver);
+    private ProjectSettingsPage projectSettingsPage = new ProjectSettingsPage(driver);
+    private GlassPage projectConfigPageGlass = new GlassPage(driver);
+
     @BeforeSuite
     public void setup() {
         System.setProperty(PageUtils.WEBDRIVER, PageUtils.CHROME_DRIVER_LOCATION);
+        loginPage.start(PageUtils.BASE_URL);
+        loginPage.login();
     }
-
-    private LoginPage loginPage = new LoginPage(driver);
 
     @Test(testName = "Glass Issue Type Schemes")
     public void glassIssueTypeSchemes() {
-        loginPage.start(PageUtils.BASE_URL);
-        loginPage.login();
+        mainPage.navigate(PageUtils.GLASS_URL);
+
+        projectConfigPageGlass.clickOnProjectSettingButton();
+        projectSettingsPage.verifyIssueTypes();
+
+        mainPage.navigate(PageUtils.GLASS_URL);
+
+        projectConfigPageGlass.clickOnSchemeTab();
+        projectConfigPageGlass.verifyScheme();
+
+        projectConfigPageGlass.clickOnIssueTypeDropdown();
+        projectConfigPageGlass.verifyIssueTypes();
 
     }
 
     @AfterSuite
     public void cleanUp() {
+        mainPage.logout();
         driver.manage().deleteAllCookies();
         driver.quit();
     }
